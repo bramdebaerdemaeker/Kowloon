3bfc5fb3ae8dc98ddd8d3dff3422dfc523ef3a9a
Bram De Baerdemaeker <bram.debaerdemaeker@gmail.com>
2017-01-08 12:04:39 +0100

    cookie design

--- kowloon.int/resources/views/layout.blade.php
+++ kowloon.int/resources/views/layout.blade.php
@@ -255,6 +255,28 @@
         </div>
     </div>
 
+    <!-- cookie overlay -->
+    <div class="cookie-screen-overlay">
+      <div class="cookie-overlay">
+
+        <!-- cookie image -->
+        <div class="cookie-image">
+          <img src="img/cookie.png" alt="cookie crumble dog bone">
+        </div>
+
+        <!-- close tag -->
+        <div class="cookie-close" id="close-cookie-overlay">
+            <i class="fa fa-times fa" aria-hidden="true"></i>
+        </div>
+
+        <div class="cookie-content">
+          <h1>Cookies</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna Duis voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+          <button type="button" name="button" class="cookie-button">ok, verder surfen</button>
+        </div>
+      </div>
+    </div>
+
 
 
 
